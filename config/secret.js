373f689dc310951b388f3e4d1666fdfb2bef6987
Suzit Kumar Libi <sujit_libi@hotmail.com>
2018-12-17 14:08:08 +0545

    Initial Commit with login and signup flow

--- config/secret.js
+++ config/secret.js
@@ -1,7 +1,7 @@
 module.exports = {
 
-  database: process.env.DATABASE || '',
+  database: process.env.DATABASE || 'mongodb://sudrishya:test12345@ds135714.mlab.com:35714/db_freelance',
   port: process.env.PORT || 3000,
-  secret: process.env.SECRET || '',
+  secret: process.env.SECRET || 'sudrishya1234',
 
 }
