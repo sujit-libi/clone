8a1842edb9c0fce8bad197776905ec05974fe407
Suzit Kumar Libi <sujit_libi@hotmail.com>
2019-01-07 19:22:27 +0545

    New Gig Post in mlab

--- routes/main.js
+++ routes/main.js
@@ -1,7 +1,45 @@
 const router = require('express').Router();
+const async = require('async');
+const Gig = require('../models/gig');
+const User = require('../models/user');
 
 router.get('/', (req, res, next) => {
     res.render('main/home');
 });
+
+router.get('/my-gigs', (req, res, next) => {
+    res.render('main/my-gigs');
+});
+
+router.route('/add-new-gig')
+    .get((req, res, next) => {
+        res.render('main/add-new-gig');
+    })
+    .post((req, res, next) => {
+        async.waterfall([
+            function (callback) {
+                var gig = new Gig();
+                gig.owner = req.user._id;
+                gig.title = req.body.gig_title;
+                gig.category = req.body.gig_category;
+                gig.about = req.body.gig_about;
+                gig.price = req.body.gig_price;
+                gig.save(function (err) {
+                    callback(err, gig);
+                })
+            },
+            function (gig, callback) {
+                User.update(
+                    {
+                        _id: req.user._id
+                    }, {
+                        $push: { gig: gig._id }
+                    }, function (err, count) {
+                        res.redirect('/my-gigs');
+                    }
+                )
+            }
+        ]);
+    });
 
 module.exports = router;
